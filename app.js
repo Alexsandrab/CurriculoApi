c05693fe8951b95d346f8f45c8fb691eabd0a81e
Alexsandrab <sandrinhaleto@hotmail.com>
2022-11-25 22:48:38 -0300

    .

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const PORT = process.env.PORT || 3001;
 const app = express();
 
 mongoose.connect(
-    process.env.MONGO_URI || "mongodb://localhost:27017/ApiRestCurriculo",
+    process.env.MONGO_URI || "mongodb://localhost:27017/ApiRestCurriculo/",
     {
         useNewUrlParser: true,
         useUnifiedTopology: true,
